c36c319562e0de1fdb32261312cf61badce8f5f3
raphaeltomagnini <raphaeltomagnini@gmail.com>
2021-09-08 20:21:26 -0300

    variaveis

---
+++ quinzena1/index.js
@@ -0,0 +1,39 @@
+
+//Exercicio 2
+
+//const primeiroNome
+//let minhaIdade
+
+//console.log(typeof primeiroNome)
+//console.log(typeof minhaIdade)
+
+// Retornou uma mensagem de erro porquê nenhum valor foi declarado para as variáveis.
+
+//const primeiroNome = prompt ("Qual é o seu nome?")
+//let minhaIdade = prompt ("Qual é a sua idade?")
+
+//console.log(typeof primeiroNome)
+//console.log(typeof minhaIdade)
+
+// O resultado das duas typeof são string. Tudo que vem de um prompt é string
+
+//console.log("Olá", primeiroNome, "você tem", minhaIdade, "anos")
+
+//const carroPreto = true
+//const carroVerde = false
+//const carroAmarelo = false
+
+//console.log("A cor do seu carro é preto?", carroPreto, "A cor do seu carro é verde?", carroVerde, "A cor do seu carro é amarelo?", carroAmarelo,)
+
+//Exercicio 3
+
+let a = 10
+let b = 25
+let c 
+c = a
+a = b
+b = c
+
+console.log("O valor de a",a)
+console.log("O valor de b",b)
+console.log("O Valor de c",a,b) 
