ba643932ef2d236a3c292b22fb66559d133cf7a9
raphaeltomagnini <raphaeltomagnini@gmail.com>
2021-11-11 19:02:34 -0300

    exercicio-react2

---
+++ jsx-labetube/props-labedin/labedin/src/components/CardPequeno.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CardPequeno.css'
+
+function CardPequeno() {
+    return (
+        <div>
+            <h4 className="email">Email: raphaeltomagnini@gmail.com</h4>
+            <h4 className="endereco">Endereço: Rua José Mendes de Carvalho, 390 apto 204 Castelo BH.MG</h4>
+            
+        </div>
+    )
+}
+
+export default CardPequeno;
