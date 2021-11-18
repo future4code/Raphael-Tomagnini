b23f664e114ab49dfb2165a660d76f872ef2bc75
raphaeltomagnini <raphaeltomagnini@gmail.com>
2021-11-17 22:09:55 -0300

    exercicio-insta4

---
+++ projeto-whatslab/app-whatslab/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <main>
+          <div className="box-texto">
+
+          </div>
+
+        </main>
+
+      
+
+    </div>
+  );
+}
+
+export default App;
