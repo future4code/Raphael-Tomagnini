66633f35c304800ace11e607fa398e0ec21f69da
raphaeltomagnini <raphaeltomagnini@gmail.com>
2022-01-25 22:20:41 -0300

    labex-atualizacao

--- projeto-labex/projeto-labex/src/pages/LoginPage.js
+++ projeto-labex/projeto-labex/src/pages/LoginPage.js
@@ -1,5 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 import { useHistory } from "react-router-dom"
+import axios from 'axios'
+import { BASE_URL } from "./utils/constants"
+
 
 export const LoginPage = () => {
     const history = useHistory()
@@ -7,14 +10,45 @@ export const LoginPage = () => {
     const goToAdminHomePage = () => {
         history.push("/adminhomepage")
     }
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChangeEmail = ({ target }) => {
+        setEmail(target.value);
+    }
+    const onChangePassword = ({ target }) => {
+        setPassword(target.value);
+
+    }    
+    const onSubmitLogin = () => {
+        axios.post(`${BASE_URL}/login`, {
+            email,
+            password
+        
+        }).then(({ data }) => {
+            console.log(data);
+        }
+            
+        ).catch(res => console.log(res));
     
+}
     return (
         <div>
             <p>Login Page</p>
-
+            <br/>
+            <p>Email</p>
+            <input value={email} onChange={onChangeEmail}/>
+            <p>Senha</p>
+            <input value={password} onChange={onChangePassword}/>
+            <br/>
+            <button onClick={onSubmitLogin}>
+                Login
+            </button>
+            <p/>
                 <button onClick={goToAdminHomePage}>
                     Ir para Página de Administrador
                 </button>
         </div>
     )
-}
+    }
